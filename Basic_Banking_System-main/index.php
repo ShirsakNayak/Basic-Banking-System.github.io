1230c505053733577000558f0107e5031f931d32
ShirsakNayak <76389210+ShirsakNayak@users.noreply.github.com>
2021-07-21 15:25:31 +0530

    Add files via upload

---
+++ Basic_Banking_System-main/index.php
@@ -0,0 +1,68 @@
+<HTML>
+<head>
+	 <meta name="author" content="SHIRSAK NAYAK">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      <link rel="stylesheet" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat|Cookie&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Delius&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" crossorigin="anonymous">
+	<center><title>Sparks Bank</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+</head>
+
+<body style="background:#6495ED;">
+	
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+	<div class="one" style=" background-color: #FF8C00; height: :50px; width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align="center"  class = "t">
+      <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2">View Transaction History</button></a></td>
+      </tr>
+      </table>
+    </table>
+	</div>
+	<div class="two">WELCOME TO</div>
+	<h1 >
+	
+<span style="color: #00008B;">S</span>
+<span style="color: #8B008B;">T</span>
+<span style="color: #9932CC;">U</span>
+<span style="color: #8B0000;">D</span>
+<span style="color: #FF1493;">E</span>
+<span style="color: #228B22;">N</span>
+<span style="color: #FF1493;">T</span>
+<span style="color: #8B0000;">B</span>
+<span style="color: #9932CC;">A</span>
+<span style="color: #8B008B;">N</span>
+<span style="color: #00008B;">K</span>
+</h1> 
+	<div class="three" >No more waiting in long queues, transfer the money very conveniently with the help of this website.  
+      
+	</div>
+
+<div class="five">Designed By SHIRSAK NAYAK<br>CONTACT ME:
+</div>
+
+      <span style="color:#FF8C00; padding: 20px;">
+
+          <br>        
+         <a style="margin-left: 600px;color:wheat;"href="#" onmouseover="this.style.color='#00FFFF'" onmouseout="this.style.color='white'" ><i class="fab fa-github fa-2x"></i></a>
+         <a style="margin-left:10px;color:wheat;"href="#" onmouseover="this.style.color='#00FFFF'" onmouseout="this.style.color='white'"><i class="fab fa-linkedin fa-2x"></i></a>
+         <a style="margin-left: 10px;color:wheat;"href="#" onmouseover="this.style.color='#00FFFF'" onmouseout="this.style.color='white'"><i class="fab fa-instagram fa-2x"></i></a>
+         
+         </span> 
+         </div>
+</body>
+</html>
